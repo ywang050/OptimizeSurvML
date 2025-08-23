2f2ec31606bb58745c549922d4d91e7ad14d1eee
ywang050 <46614270+ywang050@users.noreply.github.com>
2025-08-23 12:08:57 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+setup(
+    name='OptimizeSurvML',
+    version=0.1,
+
+    url='https://github.com/ywang050/OptimizeSurvML',
+    author='Yifan Wang',
+    author_email='ywang050@uottawa.ca',
+
+    install_requires = requirements
+)
